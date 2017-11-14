b58f0221f5bd3fc8de950b14e007b605009cf0cb
Melissa Hummelt <mkhummelt@gmail.com>
2017-11-14 21:38:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Code your solution in this file.
-function lowerCaseDrivers (drivers) {
-  return 
+function lowerCaseDrivers (list) {
+  return list.map(function (driver) {
+    return driver.toLowerCase();
+  });
 }
